35d9c7540d6b513761dbaef63ac083b4c6170e8b
sebavedoya <seba.vedoya@gmail.com>
2025-10-15 23:31:51 -0300

    Remove comments from astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  // URL pública completa de tu sitio en GitHub Pages
   site: "https://sebavedoya.github.io/Sebastian-Vedoya",
-  // subcarpeta del repo (necesario en Pages de proyecto)
   base: "/Sebastian-Vedoya",
   output: "static",
 });
